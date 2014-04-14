30163c2c4e3582a2d79c676e5dabdbe268d63786
Peter Roome <pete@wearepandr.com>
2014-04-14 22:06:28 +0100

    Pulling stocks

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,89 +2,58 @@ require 'csv'
 
 class Stock < ActiveRecord::Base
 
-  def generate_snapshot(data)
+  def self.generate_snapshot
     print "Creating new snapshot"
-    import_finviz(data)
+    @stocks = []
+    import_finviz(@stocks)
     # import_evebitda(data)
     # import_buyback_yield(data, True)
     # compute_rank(data)
-    return data
+    return @stocks
   end
 
-  def import_finviz(processed_data):
+  def self.import_finviz(processed_stocks)
     print "Importing data from finviz"
     # not using f=cap_smallover since it filters market caps over 300M instead of 200M
     # r = requests.get('http://finviz.com/export.ashx?v=152', cookies={"screenerUrl": "screener.ashx?v=152&f=cap_smallover&ft=4", "customTable": "0,1,2,6,7,10,11,13,14,45,65"})
     # r = requests.get('http://finviz.com/export.ashx?v=152', cookies={"screenerUrl": "screener.ashx?v=152&ft=4", "customTable": "0,1,2,6,7,10,11,13,14,45,65"})
-    url = 'http://finviz.com/export.ashx?v=152'
-    response = HTTParty.get(url)
-    response = CSV.parse(response)
 
-    keys = response.first
-    response = response.map {|a| Hash[ keys.zip(a) ] }
+    begin
+      url = 'http://finviz.com/export.ashx?v=152&ft=4&c=0,1,2,3,4,5,6,7,10,11,13,14,45,65'
+      response = HTTParty.get(url)
+      response = CSV.parse(response)
 
-    tickers = []
-    response.each do |row|
-      # Field labels
-      # ["No.", "Ticker", "Company", "Sector", "Industry", "Country", "Market Cap", "P/E", "Price", "Change", "Volume"]
-      unless row = response.first
-        tickers << {
-          ticker: row["Ticker"]
-          company:
-          sector:
-          industry:
-          country:
-          market_cap:
-          pe:
-          ps:
-          pb:
-          pfreecashflow:
-          dividendyield:
-          performancehalfyear:
-          price:
-        }
+      keys = response.delete_at(0).collect { |k| k.parameterize.underscore.to_sym }
+      response = response.map {|a| Hash[ keys.zip(a) ] }
+
+      response.each do |row|
+        # Field labels
+        # [:no, :ticker, :company, :sector, :industry, :country, :market_cap, :p_e, :price]
+        puts row[:ticker]
+        if row[:market_cap].to_f < 200
+          puts "Market Cap too small: #{row[:market_cap]}"
+        else
+          processed_stocks << {
+            ticker:                 row[:ticker],
+            company:                row[:company],
+            sector:                 row[:sector],
+            industry:               row[:industry],
+            country:                row[:country],
+            market_cap:             row[:market_cap],
+            p_e:                    row[:p_e].to_f,
+            p_s:                    row[:p_s].to_f,
+            p_b:                    row[:p_b].to_f,
+            p_free_cash_flow:       row[:p_free_cash_flow].to_f,
+            dividend_yield:         row[:dividend_yield].to_s.chop!.to_f,
+            performance_half_year:  row[:performance_half_year].to_s.chop!.to_f,
+            price:                  row[:price].to_f
+          }
+        end      
       end
+      puts "Finviz data imported"
+    rescue Exception => e
+      puts e.message
+      puts e.backtrace.inspect
     end
-
-    # OLD
-    
-    data = csv_to_dicts(r.text)
-    tickers = []
-    for row in data:
-      try:
-        stock = {}
-        if row["Ticker"]:
-            stock["Ticker"] = row["Ticker"]
-        print stock["Ticker"]
-        if "Importing " + row["Company"]:
-            stock["Company"] = row["Company"]
-        # Ignore companies with market cap below 200M
-        if not "Market Cap" in row or row["Market Cap"] == "":
-            continue
-        market_cap = Decimal(row["Market Cap"])
-        if market_cap < 200:
-            print "Market Cap too small: "+ row["Market Cap"]
-            continue
-        stock["MarketCap"] = row["Market Cap"]
-        if row["P/E"]:
-            stock["PE"] = row["P/E"]
-        if row["P/S"]:
-            stock["PS"] = row["P/S"]
-        if row["P/B"]:
-            stock["PB"] = row["P/B"]
-        if row["P/Free Cash Flow"]:
-            stock["PFreeCashFlow"] = row["P/Free Cash Flow"]
-        if row["Dividend Yield"]:
-            stock["DividendYield"] = row["Dividend Yield"][:-1]
-        if row["Performance (Half Year)"]:
-            stock["PerformanceHalfYear"] = row["Performance (Half Year)"][:-1]
-        if row["Price"]:
-            stock["Price"] = row["Price"]
-        processed_data[stock["Ticker"]] = stock
-    except Exception as e:
-      print e
-      #pdb.set_trace()
-    print "Finviz data imported"
   end
-
 end
